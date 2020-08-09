2192f1e119a25142492f4d4535a9f71c52a1d036
icore <icoree@gmail.com>
2020-08-09 15:29:00 +0300

    1.0

--- code.js
+++ code.js
@@ -28,18 +28,15 @@ function placeText(text) {
         figma.ui.hide();
         yield figma.loadFontAsync({ family: "Roboto", style: "Regular" });
         const nodes = [];
-        for (let i = 0; i < text.ParsedResults[0].TextOverlay.Lines["length"]; i++) {
+        for (let line of text.ParsedResults[0].TextOverlay.Lines) {
+            console.log(line);
             const rect = figma.createText();
-            rect.characters = text.ParsedResults[0].TextOverlay.Lines[i].LineText;
+            rect.characters = line.LineText;
             figma.currentPage.selection.map((selected) => {
-                rect.x =
-                    selected.width +
-                        selected.x +
-                        text.ParsedResults[0].TextOverlay.Lines[i].Words[0].Left;
+                rect.x = selected.width + selected.x + line.Words[0].Left;
             });
             figma.currentPage.selection.map((selected) => {
-                rect.y =
-                    selected.y + text.ParsedResults[0].TextOverlay.Lines[i].Words[0].Top;
+                rect.y = selected.y + line.Words[0].Top;
             });
             figma.currentPage.appendChild(rect);
             nodes.push(rect);
